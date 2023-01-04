08be05fee6774fb938ce74826c6c74b319770a7e
Markodimitrakis-Giannis <giannis.markod@gmail.com>
2023-01-04 17:47:28 +0200

    Game in it's final form

--- game.js
+++ game.js
@@ -1,31 +1,68 @@
 const startButton = (document.getElementById("start-game").onclick = playGame);
+const endButton = (document.getElementById("new-game").onclick = resetScore);
+let playerScore = 0;
+let computerScore = 0;
+const playerBox = document.querySelector(".player-stats .player h2");
+const computerBox = document.querySelector(".player-stats .computer h2");
+const playerSel = document.querySelector(".player-selections .player-sel h2");
+const computerSel = document.querySelector(
+  ".player-selections .computer-sel h2"
+);
+
+function resetScore() {
+  playerScore = 0;
+  playerBox.innerHTML = 0;
+  playerSel.innerHTML = "";
+  computerScore = 0;
+  computerBox.innerHTML = 0;
+  computerSel.innerHTML = "";
+}
 
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const playerSelection = () => {
-  return "rock";
+  const randChoice = randomInteger(0, 2);
+  const option = ["rock", "paper", "scissor"];
+  return option[randChoice];
 };
 
 const computerSelection = () => {
-  const randChoice = randomInteger(1, 3);
+  const randChoice = randomInteger(0, 2);
   const option = ["rock", "paper", "scissor"];
   return option[randChoice];
 };
 
 function playGame() {
-  console.log("Hello!");
   const playerChoice = playerSelection();
+  playerSel.innerHTML = playerChoice;
   const computerChoice = computerSelection();
+  computerSel.innerHTML = computerChoice;
 
-  const player = document.querySelector(".player-stats .player");
+  checkWinner(playerChoice, computerChoice, playerBox, computerBox);
+}
 
-  if (playerChoice == "rock" && computerChoice == "scissor") {
-    player.innerHTML("Player Won");
-  } else if (playerChoice == "rock" && computerChoice == "paper") {
-    console.log("Game Ended! Computer Won");
+function checkWinner(playerChoice, computerChoice, player, computer) {
+  if (
+    (playerChoice == "rock" && computerChoice == "scissor") ||
+    (playerChoice == "scissor" && computerChoice == "paper") ||
+    (playerChoice == "paper" && computerChoice == "rock")
+  ) {
+    console.log(playerChoice, computerChoice);
+    playerScore++;
+    player.innerHTML = playerScore;
+  } else if (
+    (playerChoice == "rock" && computerChoice == "paper") ||
+    (playerChoice == "scissor" && computerChoice == "rock") ||
+    (playerChoice == "paper" && computerChoice == "scissor")
+  ) {
+    console.log(playerChoice, computerChoice);
+    computerScore++;
+    computer.innerHTML = computerScore;
   } else {
-    console.log("Game Ended! No Player Won");
+    console.log(playerChoice, computerChoice);
+    //player.innerHTML = "Draw";
+    //computer.innerHTML = "Draw";
   }
 }
